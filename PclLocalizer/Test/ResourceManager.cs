d0a39b676b1d3e7b8e0508333f8f7836b6caebab
Marco Milani <marco.milani@giuneco.it>
2016-09-09 17:51:54 +0200

    [START]

---
+++ PclLocalizer/Test/ResourceManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    public static class ResourceManager
+    {
+        public static string Lang { get; private set; }
+
+        public static void SetLanguage(string language)
+        {
+            Lang = language;
+        }
+    }
+}
---
+++ PclLocalizer/Test/TestRes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public static class TestRes
+    {
+        private static Dictionary<string,Dictionary<string,string>> values = new Dictionary<string, Dictionary<string, string>>();
+
+        static TestRes()
+        {
+            var enDic = new Dictionary<string, string> {{"saluta", "hello!"}};
+            var itDic = new Dictionary<string, string> {{"saluta", "ciao!"}};
+
+            values.Add("en", enDic);
+            values.Add("it", itDic);
+        }
+
+        public static string Value1 => values[ResourceManager.Lang]["saluta"];
+    }
+}
